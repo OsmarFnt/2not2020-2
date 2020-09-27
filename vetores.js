0251d6da0bb6c4378891237db2f1f54989205b3b
OsmarFnt <osfon@outlook.com.br>
2020-09-27 17:48:32 +0000

    (21/08 - Aula 01)

---
+++ vetores.js
@@ -0,0 +1,17 @@
+//Declaração de vetores
+
+let vetor1 = [] //Maneira moderna
+
+//Criação de vetores com valores iniciais 
+
+let vetor3 = [2, 4, 6, 8, 10]
+let vetor4 = ['vaca', 'cachorro', 'gato', 'papagaio']
+
+//Inserção de novos valores em um vetor
+
+//Forma 1: push() - insere no final
+
+vetor1.push('laranja')
+vetor1.push('limão')
+
+console.log(vetor1)
